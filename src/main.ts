7af75d2bb2105ce32ddb7a2ca7f136de9d17128e
lustria ebiz <lustriaebiz@gmail.com>
2020-03-05 18:12:31 +0700

    done

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
+import express          from 'express';
+import express_graphql  from 'express-graphql';
+import { buildSchema }  from 'graphql';
+const app = express();
 
 class Main {
 
-    run() {
+    run(port: number) {
+
+        const schema = buildSchema(`
+            type Query {
+                message: String
+            }
+        `);
+
+        const root = {
+            message: () => JSON.stringify(['jeruk', 'apple', 'manggo'])
+        };
+
+        app.use('/graphql', express_graphql({
+            schema: schema,
+            rootValue: root,
+            graphiql: true
+        }));
+
+        app.listen(port, () => console.log(`listening on port: ${port}`));
         
     }
 
 }
 
 let main$ = new Main;
-main$.run();
+main$.run(2100);
